63c0e1de04ceed96015f169fe6e45b2ccc30fa3c
ereza <erzaamerovci@gmail.com>
2024-05-28 17:24:33 +0200

    added the delete,up and down buttons functionality

--- to-do-list/src/assets/ToDoList.jsx
+++ to-do-list/src/assets/ToDoList.jsx
@@ -1,22 +1,53 @@
 import React, { useState } from "react";
-import MainIndex from "./MainIndex.css"
+import MainIndex from "./MainIndex.css";
+
+
 function ToDoList() {
-  const [tasks, setTasks] = useState(["Eat", "Shower", "Wwalk"]);
+  const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
   }
 
-  function addTask() {}
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
 
-  function deleteTask(index) {}
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
 
-  function moveTaskUp(index) {}
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
 
-  function moveTaskDown(index) {}
+  function moveTaskDown(index) {
+    if (index < tasks.length-1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index+1]] = [
+        updatedTasks[index+1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+
+  
   return (
     <div className="to-do-list">
+
       <h1>To Do List</h1>
       <div>
         <input
@@ -29,6 +60,7 @@ function ToDoList() {
           Add
         </button>
       </div>
+   
       <ol>
         {tasks.map((task, index) => (
           <li key={index}>
@@ -45,6 +77,7 @@ function ToDoList() {
           </li>
         ))}
       </ol>
+
     </div>
   );
 }
